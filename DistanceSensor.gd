f7be5a4d0b436716b87fe0d2dbc8349581ebf42d
Ömer Faruk <omerfaruktemiz1441@gmail.com>
2021-08-15 02:00:09 +0300

    Instancing Sensors
    Still have bugs

--- DistanceSensor.gd
+++ DistanceSensor.gd
@@ -1,20 +1,23 @@
 extends Area2D
-class_name DistanceSensor
 
-export var up_sensor_range = 240
-export (String, "Only Areas", "Only Bodies", "Areas and Bodies") var up_sensor_collision_status
+var sensor_range = 240
 
 func _ready():
-	_set_ranges()
+	#_set_ranges()
+	pass
 
 func _set_ranges():
-	$UpSensor.cast_to = Vector2(0, -up_sensor_range)
+	# TODO: setting ranges according to its direction 
+	#$Ray.cast_to = Vector2(0, -sensor_range)
+	pass
+
 	
-func get_up_dist():
-	var up_sensor = get_node("UpSensor")
-	var up_collision_point = up_sensor.get_collision_point()
-	var dist = position.distance_to(up_collision_point)
-	print(dist)
+func get_dist():
+	var sensor = get_node("Ray")
+	var collision_point = sensor.get_collision_point()
+	var dist = position.distance_to(collision_point)
+	
+	return collision_point
 	
 func _physics_process(delta):
-	get_up_dist()
+	get_dist()
--- Main.gd
+++ Main.gd
@@ -1,15 +1,33 @@
 extends Node
-class_name Main
 
-onready var mob_instance = preload("res://Mob.tscn").instance()
+onready var mob_instance = preload("res://Mob.tscn")
+onready var sensor_instance = preload("res://DistanceSensor.tscn")
 #export (PackedScene) var Mob
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	new_game()
 
 func new_game():
-	var mob_st = mob_instance
-	add_child(mob_st)
-	mob_st.position = Vector2(240, 120)
+	var positions = [Vector2(240, 120), Vector2(240, 600), Vector2(80, 360), Vector2(400, 360)]
+	var cast_coordinates = [Vector2(240, 0), Vector2(240, 720), Vector2(0, 360), Vector2(480, 360)]
+	var rotations = [0, 90, 180, -90]
+	var sensor_results = []
+	
+	for i in positions:
+		var mob = mob_instance.instance()
+		add_child(mob)
+		mob.position = i
+	
+	for i in range(4):
+		var sensor = sensor_instance.instance()
+		add_child(sensor)
+		var ray = sensor.get_node("Ray")
+		ray.cast_to = cast_coordinates[i]
+		sensor.position = Vector2(240, 360)
+		sensor_results.append(sensor.get_dist())
+		
+	print(sensor_results)
+	
+	
+	
