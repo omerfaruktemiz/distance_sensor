8fab21a629c31a34326076105c5cb6189a8fc323
Ömer Faruk <omerfaruktemiz1441@gmail.com>
2021-08-23 20:25:49 +0300

    Circular Printing

--- Ray.gd
+++ Ray.gd
@@ -1,9 +1,8 @@
 extends Area2D
+export var radius = 800
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	
+onready var marker = preload("res://DetectMarker.tscn")
+
 func _physics_process(delta):
 	create_rays()
 
@@ -11,64 +10,23 @@ func create_rays():
 	var direct_space_state = get_world_2d().direct_space_state
 	var dist_list = []
 	
-	"""
-	var up_dist_list = []
-	var left_dist_list = []
-	var right_dist_list = []
-	var down_dist_list = []
-	"""
-	# Variables for calculating upside, downside, leftside and rightside distance values
-	var col_stat
-	var col_pos
-	var dist
-	
-	var radius = 800
-	
 	for theta in range(0, 1441):
-		theta = theta / 2
+		theta = theta / 4
 		var target_x = radius * cos(theta)
 		var target_y = radius * sin(theta)
 		
-		col_stat = direct_space_state.intersect_ray(position, Vector2(target_x, target_y), [self], 2147483647, true, true)
-		col_pos = col_stat.get("position")
+		var col_stat = direct_space_state.intersect_ray(position, Vector2(target_x, target_y), [self], 2147483647, true, true)
+		var col_pos = col_stat.get("position")
 		if col_pos != null:
-			dist = position.distance_to(col_pos)
+			var marker_instance = marker.instance()
+			add_child(marker_instance)
+			marker_instance.set_global_position(col_pos)
+			var dist = position.distance_to(col_pos)
+			if dist < 130:
+				marker_instance.color = Color(1, 0, 0, 1)
+			else:
+				marker_instance.color = Color(0, 1, 0, 1)
 			dist_list.append(dist)
 		
-	print(dist_list.size())	
-	
-	"""
-	# Calculating upside and downside distance values 
-	for i in range(0, 481):
-		col_stat = direct_space_state.intersect_ray(position, Vector2(i, 0), [self], 2147483647, true, true)
-		col_pos = col_stat.get("position")
-		if col_pos != null:
-			dist = position.distance_to(col_pos)
-			up_dist_list.append(dist)
-			
-		col_stat = direct_space_state.intersect_ray(position, Vector2(i, 720), [self], 2147483647, true, true)
-		col_pos = col_stat.get("position")
-		if col_pos != null:
-			dist = position.distance_to(col_pos)
-			down_dist_list.append(dist)
-	
-	# Calculating leftside and rightside distance values
-	for i in range(1, 721):
-		col_stat = direct_space_state.intersect_ray(position, Vector2(0, i), [self], 2147483647, true, true)
-		col_pos = col_stat.get("position")
-		if col_pos != null:
-			dist = position.distance_to(col_pos)
-			left_dist_list.append(dist)
-			
-		col_stat = direct_space_state.intersect_ray(position, Vector2(480, i), [self], 2147483647, true, true)
-		col_pos = col_stat.get("position")
-		if col_pos != null:
-			dist = position.distance_to(col_pos)
-			right_dist_list.append(dist)
-	
-	print(up_dist_list)
-	#print(left_dist_list.size())
-	#print(down_dist_list.size())
-	#print(right_dist_list.size())
-	"""
+	print(dist_list.size())
 	
